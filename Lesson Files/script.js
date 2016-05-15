1656de132c81ec1464c26d84d826043657cad2ba
krpinkney <15308389@massey.ac.nz>
2016-05-16 11:50:43 +1200

    Week 10

---
+++ Lesson Files/script.js
@@ -0,0 +1,28 @@
+/*
+Find out about how the JavaScript Math.sqrt() function works:
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/sqrt
+
+Features to add:
+
+- calculate the result for the hypotenuse 
+- display the hypotenuse result where it currently reads "result"
+- validate that the input values entered are greater than 0
+- display an error message for the user if the input values they are 0 or less
+
+*/
+
+document.getElementById('calculate').addEventListener('click', function() {
+  var width = document.getElementById('width').value;
+  var height = document.getElementById('height').value;
+  
+  if (width > 0 && height > 0) {
+    document.getElementById('triangle').setAttribute('width', width);
+    document.getElementById('triangle').setAttribute('height', height);
+    var result = Math.sqrt(height*height + width*width);
+    document.getElementById('hypotenuse').value = result;
+    }
+  else {
+    alert("Please enter values that are greater than 0");
+    document.getElementById('hypotenuse').value = 'result';
+  }
+});
